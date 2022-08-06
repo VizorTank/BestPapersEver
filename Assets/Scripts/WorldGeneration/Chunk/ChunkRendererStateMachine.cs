fad41a2ab548569df371a9f44c3b84fed2af28e3
VizorTank <vizortank@wp.pl>
2022-08-06 13:53:08 +0200

    Yes (too many changes)

---
+++ Assets/Scripts/WorldGeneration/Chunk/ChunkRendererStateMachine.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class ChunkRendererStateMachine
+{
+    public void CopyBlocks()
+    {
+        
+    }
+    
+    public void CreateClusters()
+    {
+        
+    }
+
+    public void CheckClusterVisibility()
+    {
+        
+    }
+
+    public void CreateMeshDataWithClusters()
+    {
+        
+    }
+
+    public Mesh CreateMesh()
+    {
+        return null;
+    }
+}
